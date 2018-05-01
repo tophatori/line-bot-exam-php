7c7a0b11b731e09eda0b39dc3c2759c93700bf67
tophatori <likhittop@gmail.com>
2018-05-02 02:32:42 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,10 +1,9 @@
 <?php
 
 
-$access_token = 'roMPb5NmOWgQ8S0j/Ozs9/L/Gr8fiyNYDz9hJEGZJhQub76bsCgrq5k5ESsvltErm0IwK3HJZ/7Uh/YDH3DFU/gaBek2kySbx/w7/+F8w+FqMdL3EQ0jZdvwXCoNQCWZFeq2pOVyLtlLG7CB3v2EcwdB04t89/1O/w1cDnyilFU=
-';
+$access_token = 'roMPb5NmOWgQ8S0j/Ozs9/L/Gr8fiyNYDz9hJEGZJhQub76bsCgrq5k5ESsvltErm0IwK3HJZ/7Uh/YDH3DFU/gaBek2kySbx/w7/+F8w+FqMdL3EQ0jZdvwXCoNQCWZFeq2pOVyLtlLG7CB3v2EcwdB04t89/1O/w1cDnyilFU=';
 
-$userId = 'Uffa138efe037e6e889d0b0f4a871c005';
+$userId = 'U866aacc04480721fce435195aa42a9c6';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
